c13d5074d9c053811fcc29d1c0ea84f8db000e3f
Notna17 <tonketi2007@gmail.com>
2025-04-14 14:01:52 +0300

    Зробив каркас проекту (Юра закоміть ту функцію)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Hello");
+const hello = console.log("Hello");
+const lol = document.getElementById("lol");
+lol.addEventListener("click", hello);
+// ось як працюють event listener і document.getElementById
