b3033baab97509873206a6b2852b2d6bcd76df22
Notna17 <tonketi2007@gmail.com>
2025-04-22 14:54:06 +0300

    Додав функцію для лаби 3

--- src/handyFunctions.js
+++ src/handyFunctions.js
@@ -7,3 +7,30 @@ const deArraify = (word) => {
     if (typeof word === "string") {return word}
     return word.join("")
 };
+
+const memoize = (f, n) => { // Лабораторна робота 3, будемо використовувати щоб мемоїзувати перевірку дійсності слова
+    const memory = new Map(); // У Мапі формат зберігання input: [answer, counter]
+    return (...args) => {
+        const input = args.join(",");
+        if (memory.has(input)) {
+            const arr = memory.get(input);
+            memory.set(input, [arr[0], arr[1] + 1]);
+            return arr[0];
+        } else {
+            if (memory.size >= n) {
+                let minKey;
+                let minCount = Infinity;
+                for (const [key, arr] of memory.entries()) {
+                    if (arr[1] < minCount) {
+                        minCount = arr[1];
+                        minKey = key;
+                    }
+                }
+                memory.delete(minKey);
+            };
+            const answer = f(...args);
+            memory.set(input, [answer, 1]);
+            return answer;
+        }
+    }
+};
