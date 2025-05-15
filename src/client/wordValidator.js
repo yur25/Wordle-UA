d71f31e9749226997524b436093a7d16408fccca
Yurii <yurachoruk123@gmail.com>
2025-05-15 15:26:16 +0300

    додав функцію, яка перевіряє чи слово присутнє в файлі

--- src/client/wordValidator.js
+++ src/client/wordValidator.js
@@ -1,22 +1,15 @@
 const fs = require('node:fs');
 const readline = require('node:readline');
 
-async function getRandomWord(filePath) {
+async function validateWord(filePath, word) {
   const fileStream = fs.createReadStream(filePath);
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
 
-  let selectedLine = null;
-  let count = 0;
-
   for await (const line of rl) {
-    count++;
-    if (Math.random() < 1 / count) {
-      selectedLine = line;
-    }
+    if (line === word) return true;
   }
-
-  return selectedLine;
+  return false;
 }
