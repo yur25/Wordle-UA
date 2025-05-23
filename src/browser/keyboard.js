efe48b88073cd71b18a73cb58428b4dd720cce6e
Notna17 <tonketi2007@gmail.com>
2025-05-23 11:09:46 +0300

    Зробив код створення клавіатури гарнішим

--- src/browser/keyboard.js
+++ src/browser/keyboard.js
@@ -1,39 +1,23 @@
-const keyrowArray0 = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ї'];
-const keyrowArray1 = ['ф', 'і', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'є', '←'];
-const keyrowArray2 = ["'",'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', 'ґ', '✔'];
+const keyboardArray = [
+    ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ї'],
+    ['ф', 'і', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'є', '←'],
+    ["'",'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', 'ґ', '✔']
+];
 
-let keyrow0 = document.getElementById('keyrow-0');
-    keyrowArray0.forEach((value) => {
+for (let i = 0; i < keyboardArray.length; i++) {
+    const keyrow = document.getElementById('keyrow-' + i.toString());
+    keyboardArray[i].forEach((value) => {
         const keyElement = document.createElement('div');
         keyElement.classList.add('keyboard-box');
         keyElement.innerText = value;
         keyElement.id = 'keybox-' + value;
-        keyrow0.appendChild(keyElement);
-        keyElement.addEventListener('click', normalKey(value));
-    });
-let keyrow1 = document.getElementById('keyrow-1');
-    keyrowArray1.forEach((value) => {
-        const keyElement = document.createElement('div');
-        keyElement.classList.add('keyboard-box');
-        keyElement.innerText = value;
-        keyElement.id = 'keybox-' + value;
-        keyrow1.appendChild(keyElement);
+        keyrow.appendChild(keyElement);
         if (value === '←') {
             keyElement.addEventListener('click', eraseKey);
-        } else {
-            keyElement.addEventListener('click', normalKey(value));
-        }
-    });
-let keyrow2 = document.getElementById('keyrow-2');
-    keyrowArray2.forEach((value) => {
-        const keyElement = document.createElement('div');
-        keyElement.classList.add('keyboard-box');
-        keyElement.innerText = value;
-        keyElement.id = 'keybox-' + value;
-        keyrow2.appendChild(keyElement);
-        if (value === '✔') {
+        } else if (value === '✔') {
             keyElement.addEventListener('click', submitKey);
         } else {
             keyElement.addEventListener('click', normalKey(value));
         }
     });
+}
