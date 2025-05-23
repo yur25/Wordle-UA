0025746bee8033bf7e2978018e6a9b80916eba56
Notna17 <tonketi2007@gmail.com>
2025-05-23 15:45:51 +0300

    Начепив memoize на validateWord

--- src/browser/handyFunctions.js
+++ src/browser/handyFunctions.js
@@ -10,32 +10,6 @@ const deArraify = (word) => {
 
 const getBox = (i, j) => { return document.getElementById('box' + i.toString() + '-' + j.toString()) }
 
-const memoize = (f, n) => { // Лабораторна робота 3, будемо використовувати щоб мемоїзувати перевірку дійсності слова
-    const memory = new Map(); // У Мапі формат зберігання input: [answer, counter]
-    return (...args) => {
-        const input = args.join(",");
-        if (memory.has(input)) {
-            const arr = memory.get(input);
-            memory.set(input, [arr[0], arr[1] + 1]);
-            return arr[0];
-        }
-        if (memory.size >= n) {
-            let minKey;
-            let minCount = Infinity;
-            for (const [key, arr] of memory.entries()) {
-                if (arr[1] < minCount) {
-                        minCount = arr[1];
-                        minKey = key;
-                }
-            }
-                memory.delete(minKey);
-        };
-        const answer = f(...args);
-        memory.set(input, [answer, 1]);
-        return answer;
-    }
-};
-
 const logDecorator = (f, logger = console.log) => { // Лабораторна робота 9
     const name = f.name;
     return (logLevel, ...args) => { // logLevel INFO буде видавати загальну інформацію, DEBUG усю, ERROR - лише помилки, будь що інше ніякої інформації
